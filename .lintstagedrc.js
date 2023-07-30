4b8786cbe057bf1284543b8060c41f63be9ec621
chansyawn <chansyawn@gmail.com>
2023-07-30 22:34:19 +0800

    build: prettier in pre-commit only work with file in specified format

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,5 +5,5 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-  "*": "prettier --ignore-unknown --write",
+  "*.{js,jsx,ts,tsx,json}": "prettier --write",
 };
