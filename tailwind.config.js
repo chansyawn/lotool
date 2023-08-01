ef99aa7be3655d5f28f024646ef465a035154143
chansyawn <chansyawn@gmail.com>
2023-08-01 22:22:37 +0800

    feat: adjust responsive screen config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
   plugins: [],
 };
