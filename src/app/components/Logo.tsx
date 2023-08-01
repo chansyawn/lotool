b5536fae7a877e860e88e04273679feb10ed158a
chansyawn <chansyawn@gmail.com>
2023-08-01 22:49:35 +0800

    feat: add space between icon and text

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <span className="py-2 text-2xl">
-      <Link href="/">📦Tool</Link>
+      <Link href="/">
+        📦<span className="ml-1">Tool</span>
+      </Link>
     </span>
   );
 }
