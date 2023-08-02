2b356f4c7bd0445502f1d326f7bc111fac0ffdb3
chansyawn <chansyawn@gmail.com>
2023-08-02 22:16:39 +0800

    fix: prevent scroll when adjust number input by wheel event

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,28 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+"use client";
+
+import { DetailedHTMLProps, InputHTMLAttributes, useEffect, useRef } from "react";
 import clsx from "clsx";
 
 type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {};
 
-export default function Input({ className, ...props }: InputProps) {
+export default function Input({ className, type, ...props }: InputProps) {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const inputDom = ref.current;
+    if (!inputDom || type !== "number") return;
+
+    const preventWheel: EventListener = (e) => e.stopPropagation();
+    inputDom.addEventListener("wheel", preventWheel);
+
+    return () => inputDom.removeEventListener("wheel", preventWheel);
+  }, [type]);
+
   return (
     <input
+      ref={ref}
       className={clsx("rounded border px-2 py-1 outline-2 outline-neutral-400 focus:outline", className)}
+      type={type}
       {...props}
     />
   );
