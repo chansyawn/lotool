b0e9a9dbea750bfec763167a61eea08eda5ee2db
chansyawn <chansyawn@gmail.com>
2023-08-02 20:41:03 +0800

    fix: set real time in useEffect to avoid hydration issue

--- src/app/timestamp/components/RealTime.tsx
+++ src/app/timestamp/components/RealTime.tsx
@@ -3,16 +3,17 @@
 import { useEffect, useState } from "react";
 
 export default function RealTime() {
-  const [currTimestamp, setCurrTimestamp] = useState(new Date().valueOf());
+  const [currTimestamp, setCurrTimestamp] = useState(0);
   const currTimestampSeconds = (currTimestamp / 1000).toFixed();
 
   useEffect(() => {
+    setCurrTimestamp(new Date().valueOf());
     const timeout = setInterval(() => {
       setCurrTimestamp(new Date().valueOf());
     }, 1000);
 
     return () => clearInterval(timeout);
-  });
+  }, []);
 
   return (
     <>
