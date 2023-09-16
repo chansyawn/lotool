8917bb5b17dce861bf1e57282a9902c9cd742f1e
chansyawn <chansyawn@gmail.com>
2023-09-16 19:03:24 +0800

    feat: dynamic relative time

--- src/app/timestamp/components/RelativeTime.tsx
+++ src/app/timestamp/components/RelativeTime.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import { formatRelative } from "date-fns";
+import { formatDistance } from "date-fns";
+import { useEffect, useState } from "react";
 
 type RelativeTimeProps = {
   timestamp: number;
 };
 
 export function RelativeTime({ timestamp }: RelativeTimeProps) {
-  return <div>({formatRelative(timestamp, new Date())})</div>;
+  const [relativeTime, setRelativeTime] = useState("");
+
+  useEffect(() => {
+    setRelativeTime(getRelativeTime(timestamp));
+    const updateInterval = setInterval(() => {
+      setRelativeTime(getRelativeTime(timestamp));
+    }, 1000);
+
+    return () => clearInterval(updateInterval);
+  }, [timestamp]);
+
+  return <span>({relativeTime})</span>;
 }
+
+const getRelativeTime = (timestamp: number) => {
+  return formatDistance(timestamp, new Date(), { addSuffix: true });
+};
