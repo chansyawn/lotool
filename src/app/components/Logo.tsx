8ec9a21f5fb6ebe2757e5c02cba8797e6dfe7b7d
chansyawn <chansyawn@gmail.com>
2023-09-17 19:50:17 +0800

    feat: add falsework entrance in logo

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
 
 export default function Logo() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleLogoClick: MouseEventHandler = (e) => {
+    if (pathname === "/falsework") {
+      e.preventDefault();
+    }
+    if (e.detail >= 5) {
+      e.preventDefault();
+      router.push("/falsework");
+    }
+  };
+
   return (
     <span className="py-2 text-2xl">
       <Link href="/">
-        📦<span className="ml-1">Lotool</span>
+        <span onClick={handleLogoClick}>📦</span>
+        <span className="ml-1">Lotool</span>
       </Link>
     </span>
   );
