f07805ddd22d2f710207c7dc6bfcaf0849372d33
chansyawn <chansyawn@gmail.com>
2023-10-21 09:36:52 +0800

    feat: add svgr

--- src/layouts/tool/RelatedLinkIcon.tsx
+++ src/layouts/tool/RelatedLinkIcon.tsx
@@ -1,10 +1,9 @@
 import { LinkIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
 import { RelatedLinkType } from "./RelatedLink";
 import WikipediaIcon from "@/icons/wikipedia.svg";
 
 export const LinkIconMap: Record<RelatedLinkType, React.ReactNode> = {
-  wiki: <Image src={WikipediaIcon} className="h-4 w-4" alt="Wikipedia" />,
+  wiki: <WikipediaIcon className="h-4 w-4 fill-current" />,
 };
 
 type RelatedLinkIconProps = {
