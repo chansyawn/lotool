fc3ab8502460ec12bc0dd4712d655a2dbd9f4688
chansyawn <chansyawn@gmail.com>
2023-10-22 22:21:42 +0800

    fix: recover implement way of favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,15 +10,13 @@ import { initColorModeScript } from "@/styles/colorMode";
 export const metadata: Metadata = {
   title: "Lotool",
   description: "A Toolbox",
-  icons: {
-    icon: getFaviconHrefByText("📦"),
-  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href={getFaviconHrefByText("📦")} />
         <script id="set-theme" dangerouslySetInnerHTML={{ __html: initColorModeScript }} />
       </head>
       <body className={cn(...fonts.map((font) => font.variable))}>
