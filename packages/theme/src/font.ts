a7c8a0aa9091d2ea3deb37e6ac3468482dca2def
chansyawn <chansyawn@gmail.com>
2024-03-29 22:12:30 +0800

    fix: use correct font config

--- packages/theme/src/font.ts
+++ packages/theme/src/font.ts
@@ -4,12 +4,14 @@ const RobotoFlex = RobotoFlexFont({
   subsets: ["latin"],
   variable: "--font-roboto-flex",
   fallback: ["sans-serif"],
+  display: "swap",
 });
 
 const RobotoMono = RobotoMonoFont({
   subsets: ["latin"],
   variable: "--font-roboto-mono",
   fallback: ["monospace"],
+  display: "swap",
 });
 
 export const fonts = [RobotoFlex, RobotoMono];
