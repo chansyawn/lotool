0461342ccf3ff13893dbe6249c2286c0d406e532
chansyawn <chansyawn@gmail.com>
2024-06-01 19:09:57 +0800

    feat: reset tool atom when route change

--- apps/web/src/app/tools/_layout/index.tsx
+++ apps/web/src/app/tools/_layout/index.tsx
@@ -12,6 +12,7 @@ import {
 import React from "react";
 import { Portal } from "@radix-ui/react-portal";
 import { PictureInPictureIcon } from "lucide-react";
+import { Provider } from "jotai";
 import { useDocumentPiPContext } from "@/features/document-pip/document-pip-context";
 import { TOOL_CONFIG } from "../config";
 import { References } from "./references";
@@ -40,40 +41,42 @@ function ToolLayout({ children }: ToolProps) {
   const { name, relatedLink } = tool;
 
   return (
-    <div className="px-4">
-      <div className="flex-grow">
-        <h1 className="mb-4 flex items-center text-3xl font-semibold">
-          <ToolIcon className="mr-2 size-6" name={name} path={path} />
-          <span>{name}</span>
-          {isSupportDocumentPiP && !documentPiPWindow ? (
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button variant="ghost" size="icon" className="ml-auto" onClick={openPiPWindow}>
-                  <PictureInPictureIcon className="size-4" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>Open in Picture-in-Picture mode(Beta)</TooltipContent>
-            </Tooltip>
-          ) : null}
-        </h1>
-        {documentPiPWindow ? (
-          <PortalContainerProvider container={documentPiPWindow.document.body}>
-            <Portal container={documentPiPWindow.document.body} className="p-4">
-              {children}
-            </Portal>
-            <div className="flex aspect-video flex-col items-center justify-center">
-              <div className="mb-2 text-xl font-semibold">Picture in Picture Mode Activated</div>
-              <Button onClick={closePiPWindow}>Click to close</Button>
-            </div>
-          </PortalContainerProvider>
-        ) : (
-          children
-        )}
+    <Provider key={path}>
+      <div className="px-4">
+        <div className="flex-grow">
+          <h1 className="mb-4 flex items-center text-3xl font-semibold">
+            <ToolIcon className="mr-2 size-6" name={name} path={path} />
+            <span>{name}</span>
+            {isSupportDocumentPiP && !documentPiPWindow ? (
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant="ghost" size="icon" className="ml-auto" onClick={openPiPWindow}>
+                    <PictureInPictureIcon className="size-4" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Open in Picture-in-Picture mode(Beta)</TooltipContent>
+              </Tooltip>
+            ) : null}
+          </h1>
+          {documentPiPWindow ? (
+            <PortalContainerProvider container={documentPiPWindow.document.body}>
+              <Portal container={documentPiPWindow.document.body} className="p-4">
+                {children}
+              </Portal>
+              <div className="flex aspect-video flex-col items-center justify-center">
+                <div className="mb-2 text-xl font-semibold">Picture in Picture Mode Activated</div>
+                <Button onClick={closePiPWindow}>Click to close</Button>
+              </div>
+            </PortalContainerProvider>
+          ) : (
+            children
+          )}
+        </div>
+        <aside className="mt-4 flex-shrink-0">
+          {relatedLink ? <References links={relatedLink} /> : null}
+        </aside>
       </div>
-      <aside className="mt-4 flex-shrink-0">
-        {relatedLink ? <References links={relatedLink} /> : null}
-      </aside>
-    </div>
+    </Provider>
   );
 }
 
